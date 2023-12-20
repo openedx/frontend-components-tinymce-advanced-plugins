f4094bcc9eb1cbf7664e98d6a4dc8acd38788645
Ken Clary <kclary@edx.org>
2023-12-20 15:19:55 -0500

    fix: more correct(?) basic plugin stubs

--- plugins/a11ychecker/plugin.js
+++ plugins/a11ychecker/plugin.js
@@ -1,11 +1,9 @@
 import tinymce from 'tinymce';
 
 // eslint-disable-next-line no-unused-vars
-tinymce.PluginManager.add('a11ychecker', (editor, url) => ({
-  getMetadata() {
-    return {
-      name: 'stub open-source accessibility checker',
-      url: 'https://github.com/openedx/frontend-components-tinymce-advanced-plugins',
-    };
-  },
-}));
+tinymce.PluginManager.add('a11ychecker', (editor, url) => {
+  return {
+    name: 'stub open-source accessibility checker',
+    url: 'https://github.com/openedx/frontend-components-tinymce-advanced-plugins',
+  };
+});
--- plugins/powerpaste/plugin.js
+++ plugins/powerpaste/plugin.js
@@ -1,11 +1,9 @@
 import tinymce from 'tinymce';
 
 // eslint-disable-next-line no-unused-vars
-tinymce.PluginManager.add('powerpaste', (editor, url) => ({
-  getMetadata() {
-    return {
-      name: 'stub open-source powerpaste',
-      url: 'https://github.com/openedx/frontend-components-tinymce-advanced-plugins',
-    };
-  },
-}));
+tinymce.PluginManager.add('powerpaste', (editor, url) => {
+  return {
+    name: 'stub open-source powerpaste',
+    url: 'https://github.com/openedx/frontend-components-tinymce-advanced-plugins',
+  };
+});
