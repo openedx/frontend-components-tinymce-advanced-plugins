ee73a4bb0637a08e1130062d984b4300e355cdc3
Ken Clary <kclary@edx.org>
2023-12-20 15:41:07 -0500

    fix: more correct(?) basic plugin stubs

--- plugins/a11ychecker/plugin.js
+++ plugins/a11ychecker/plugin.js
@@ -1,11 +1,15 @@
+/* eslint-disable */
 import tinymce from 'tinymce';
 
-// eslint-disable-next-line no-unused-vars
-tinymce.PluginManager.add('a11ychecker', (editor, url) => ({
-  getMetadata() {
-    return {
-      name: 'stub open-source accessibility checker',
-      url: 'https://github.com/openedx/frontend-components-tinymce-advanced-plugins',
-    };
-  },
-}));
+// disabling lint is necessary, because of the prefer-arrow-callback rule causing the below code to get
+// rewritten to a broken state
+tinymce.PluginManager.add('a11ychecker', function(editor, url) {
+  return {
+    getMetadata: function() {
+      return {
+        name: 'stub open-source accessibility checker',
+        url: 'https://github.com/openedx/frontend-components-tinymce-advanced-plugins',
+      };
+    },
+  };
+});
